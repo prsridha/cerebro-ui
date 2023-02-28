f05f35a4e6cc6a4554b193b0e26e1fdc41413f8b
prsridha <prsridha@ucsd.edu>
2023-02-27 17:38:30 -0800

    dashboard backend working

--- src/app/dashboards/dashboards.component.ts
+++ src/app/dashboards/dashboards.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface DashboardURLs {
+  grafanaURL: string;
+  jupyterURL: string;
+  tensorboardURL: string;
+}
 
 @Component({
   selector: 'app-dashboards',
   templateUrl: './dashboards.component.html',
   styleUrls: ['./dashboards.component.css']
 })
-export class DashboardsComponent {
 
+export class DashboardsComponent implements OnInit{
+  constructor(private httpClient: HttpClient){}
+  urls = <DashboardURLs> {};
+
+  ngOnInit() {
+    var baseURL = "http://localhost:8080";
+    
+    this.httpClient.get(baseURL + '/get-urls').subscribe((res: any)=>{
+      this.urls.grafanaURL = res.message["grafanaURL"];
+      this.urls.jupyterURL = res.message["jupyterURL"];
+      this.urls.tensorboardURL = res.message["tensorboardURL"];
+      console.log(this.urls);
+    });
+  }
+
+  goToLink(url: string){
+    window.open(url, "_blank");
+  };
 }
