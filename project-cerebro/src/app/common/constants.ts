91846ff4b55c8e6190758380b63ea03fa6c5e3ab
prsridha <prsridha@ucsd.edu>
2023-02-28 03:00:29 -0800

    hard code backend url

--- project-cerebro/src/app/common/constants.ts
+++ project-cerebro/src/app/common/constants.ts
@@ -1,3 +1,3 @@
 export class GlobalConstants {
-    public static backendURL: string = "http://localhost:8080";
+    public static backendURL: string = "http://ec2-35-183-8-210.ca-central-1.compute.amazonaws.com:30083";
 }
